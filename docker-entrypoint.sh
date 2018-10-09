59ef5f0eaebb0df79de3ceb775ec1a5ce4c442de
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2018-10-09 14:49:58 +0300

    add cat to the cron instance

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -16,6 +16,7 @@ chown 500:500 /var/local/groupware/var/naaya-mail-queue/tmp
 if [ ! -z "$CRONTAB" ]; then
   crontab -u zope crontab.cfg
   crond
+  cat
 else
   if [[ $START == *"$1"* ]]; then
     _stop() {
