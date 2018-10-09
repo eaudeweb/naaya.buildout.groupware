11ffb37f5206f9f8dd6decd44fc1e142652bd957
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2018-10-09 14:04:02 +0300

    cron instance should not start as zope

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -16,29 +16,25 @@ chown 500:500 /var/local/groupware/var/naaya-mail-queue/tmp
 if [ ! -z "$CRONTAB" ]; then
   crontab -u zope crontab.cfg
   crond
-fi
-
-if [[ $START == *"$1"* ]]; then
-  _stop() {
-    bin/instance stop
-    kill -TERM $child 2>/dev/null
-  }
-
-  trap _stop SIGTERM SIGINT
-
-  if [ ! -z "$DEBUG_MODE" ]; then
-    bin/develop up
-    git --git-dir=src/naaya/.git pull
-  fi
-
-  bin/instance start
-  bin/instance logtail &
-
-  child=$!
-  wait "$child"
 else
-  if [[ $COMMANDS == *"$1"* ]]; then
-    exec bin/instance "$@"
+  if [[ $START == *"$1"* ]]; then
+    _stop() {
+      bin/instance stop
+      kill -TERM $child 2>/dev/null
+    }
+    trap _stop SIGTERM SIGINT
+    if [ ! -z "$DEBUG_MODE" ]; then
+      bin/develop up
+      git --git-dir=src/naaya/.git pull
+    fi
+    bin/instance start
+    bin/instance logtail &
+    child=$!
+    wait "$child"
+  else
+    if [[ $COMMANDS == *"$1"* ]]; then
+      exec bin/instance "$@"
+    fi
+    exec "$@"
   fi
-  exec "$@"
 fi
